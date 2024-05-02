a78b3e085475dc78582b2da370e76b5593f0838d
Erfi Anugrah <erfi.anugrah@gmail.com>
2024-05-02 11:00:43 +0000

    refactored subnet split code

--- Misc/list_all_ips_prefix.py
+++ Misc/list_all_ips_prefix.py
@@ -1,10 +1,16 @@
 import ipaddress
 
-# Define the /8 network
-network = ipaddress.IPv4Network('154.0.0.0/8')
+# Prompt the user for the private network
+network_str = input("Please enter the private network range that you want to split (e.g., 154.0.0.0/8): ")
 
-# Generate /25 subnets
-subnets = list(network.subnets(new_prefix=25))
+# Convert the input to an IPv4Network object
+network = ipaddress.IPv4Network(network_str)
+
+# Prompt the user for the new subnet prefix
+new_prefix = int(input("Please enter the new subnet prefix (e.g., 25): "))
+
+# Generate the subnets
+subnets = list(network.subnets(new_prefix=new_prefix))
 
 # Format the subnets with double quotes and commas, and enclose in square brackets
 subnets_str = '[' + ', '.join(f'"{subnet}"' for subnet in subnets) + ']'
